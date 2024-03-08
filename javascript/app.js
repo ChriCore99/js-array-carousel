b8b56c5c5a3cc0275b51b2e099d547af771f15e5
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-08 17:15:45 +0100

    ADD: aggiunta immagine da js

--- javascript/app.js
+++ javascript/app.js
@@ -5,3 +5,28 @@ const caroselloImmagini = [
     './img/04.webp', 
     './img/05.webp'
 ];
+
+const imagesContainerDOMElement = document.querySelector('.images-container');
+
+imagesContainerDOMElement.innerHTML =`<div class="foto-sfondo">
+<img src="./img/01.webp" alt="">
+</div>`;
+
+
+
+
+
+
+
+
+const btnScorrimentoSu = document.querySelector('.btn-su');
+
+const btnScorrimentoGiu = document.querySelector('.btn-giu');
+
+btnScorrimentoSu.addEventListener('click', function(){
+    console.log('click div su');
+})
+
+btnScorrimentoGiu.addEventListener('click', function(){
+    console.log('click div giu');
+})
